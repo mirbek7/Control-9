d0d675df0021dbc0b8808b976a45bdf7dea74bd8
Mirbek Shamenov <Shamenovm87@gmail.com>
2024-10-05 12:29:14 +0600

    Add Maven wrapper and initial Spring Boot project setup
    Added Maven wrapper scripts and properties for consistent build environment. Initialized Spring Boot project with basic configuration in `pom.xml`, application properties, and essential `.gitignore` entries.

---
+++ src/main/java/kg/atractor/control9/Control9Application.java
@@ -0,0 +1,13 @@
+package kg.atractor.control9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control9Application.class, args);
+	}
+
+}
---
+++ src/test/java/kg/atractor/control9/Control9ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.atractor.control9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
